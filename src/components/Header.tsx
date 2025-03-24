b3d66ca53fd4a363434b4d58723bad7dbd25f7a7
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-03-24 11:00:49 -0500

    add: Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+// Styles
+import "../assets/css/components/Header.css";
+
+// Images - icons
+import Logo from "../assets/img/Logo.png";
+
+// React icons
+import { RiMenu3Line } from "react-icons/ri";
+
+const Header = () => {
+    const [ menu, setMenu ] = useState(false)
+
+    const handleMenu = () => {
+        setMenu(!menu)
+    }
+
+    return (
+        <header className="header">
+            <ul className="navbar">
+                <li className="navbar-item">Inicio</li>
+                <li className="navbar-item">Acerca de</li>
+                <li className="navbar-item navbar-item-img">
+                    <img src={Logo} alt="" />
+                </li>
+                <li className="navbar-item">Episodios</li>
+                <li className="navbar-item">Contacto</li>
+                <li className="navbar-item navbar-item-menu">
+                    <button className="btn-menu" onClick={handleMenu}>
+                        <RiMenu3Line />
+                    </button>
+                </li>
+            </ul>
+            <ul className={ `navbar-mobile ${menu ? "active" : ""}` }>
+                <h2>Menú:</h2>
+                <li className="navbar-mobile-item">Inicio</li>
+                <li className="navbar-mobile-item">Acerca de</li>
+                <li className="navbar-mobile-item">Episodios</li>
+                <li className="navbar-mobile-item">Contacto</li>
+            </ul>
+        </header>
+    )
+}
+
+export { Header };
