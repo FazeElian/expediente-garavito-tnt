2c7933e51a16d07b3ea81842b79c7c8b98b06e2e
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-04-16 11:26:02 -0500

    feat: Animation to continue to form on share story

--- src/views/ShareStoryView.tsx
+++ src/views/ShareStoryView.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 // Styles
 import { Link } from "react-router-dom";
 import "../assets/css/components/StoryForm.css";
@@ -5,8 +7,25 @@ import "../assets/css/components/StoryForm.css";
 // React icons
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { IoWarningOutline } from "react-icons/io5";
+import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 
 const ShareStoryView = () => {
+    const [ intro, setIntro ] = useState(true);
+    const [ btnContinue, setBtnContinue ] = useState(false);
+
+    useEffect(() => {
+        const timeOut = setTimeout(() => {
+            setBtnContinue(true)
+        }, 5000)
+
+        return () => clearTimeout(timeOut)
+    }, [])
+
+    const disableIntro = () => {
+        setIntro(false)
+        setBtnContinue(false)
+    }
+
     return (
         <section className="share-story">
             <div className="top-share-story">
@@ -14,7 +33,7 @@ const ShareStoryView = () => {
                     <FaArrowLeftLong />
                 </Link>
             </div>
-            <div className="warning-story">
+            <div className={`warning-story ${intro ? "" : "disabled"}`}>
                 <div className="warning-icon">
                     <IoWarningOutline />
                 </div>
@@ -24,6 +43,10 @@ const ShareStoryView = () => {
                     Lo inexplicable, lo aterrador, lo sobrenatural... todo es bienvenido aquí.
                 </p>
             </div>
+            <button className={ `btn-continue ${btnContinue ? "" : "active"}` } onClick={disableIntro}>
+                Continuar
+                <MdOutlineKeyboardDoubleArrowRight />
+            </button>
         </section>
     )
 }
