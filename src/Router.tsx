4bc492038ab52856add5270161ec49a551b3d869
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-04-17 19:43:22 -0500

    perf: Views optimization with lazy loading

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,25 +1,28 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // Header component
 import { Header } from "./components/Header";
 
 // Views components
-import HomeView from "./views/HomeView";
-import BlogView from "./views/BlogView";
-import ShareStoryView from "./views/ShareStoryView";
-import StoryView from "./views/StoryView";
+const HomeView = lazy(() => import("./views/HomeView"));
+const BlogView = lazy(() => import("./views/BlogView"));
+const ShareStoryView = lazy(() => import("./views/ShareStoryView"));
+const StoryView = lazy(() => import("./views/StoryView"));
 
 function Router() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route element={<Header />}>
-                    <Route index element={<HomeView />} />
-                    <Route path="/blog" element={<BlogView />} />
-                </Route>
-                <Route path="/share-story" element={<ShareStoryView />} /> 
-                <Route path="/blog/:id" element={<StoryView />} /> 
-            </Routes>
+            <Suspense fallback={<div>Cargando...</div>}>
+                <Routes>
+                    <Route element={<Header />}>
+                        <Route index element={<HomeView />} />
+                        <Route path="/blog" element={<BlogView />} />
+                    </Route>
+                    <Route path="/share-story" element={<ShareStoryView />} /> 
+                    <Route path="/blog/:id" element={<StoryView />} /> 
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     )
 }
